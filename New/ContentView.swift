1081fde41c102d1e5869094ac1f25442084e0f51
GhaliahAltoom <ghaliah.altoom@outlook.com>
2023-10-05 12:46:58 +0300

    Initial Commit

---
+++ New/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New
+//
+//  Created by Ghaliah Altoom on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New/NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApp.swift
+//  New
+//
+//  Created by Ghaliah Altoom on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
